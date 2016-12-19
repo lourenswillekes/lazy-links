85be26b38cf11aa72f08744b79c376b48fc96360
lourenswillekes <lourw95@gmail.com>
2016-12-18 23:36:29 -0500

    append numbers to links

--- content.js
+++ content.js
@@ -3,7 +3,8 @@
  * December 2016
  ************************************************************************/
 
-var elements = document.getElementsByTagName('a');
+
+ var elements = document.getElementsByTagName('a');
 	// <a href="/PROD/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu"  class="submenulinktext2" id="ssbbackurl">RETURN TO MENU</a>/ 
 	// <a href="http://www.travel-explorer.com/australia-best-beaches/" target="_blank"><img src="/pix/byron_bay_225x169.jpg" style="border:1px solid black;max-width:100%;" alt="Photo of Byron Bay, one of Australia's best beaches!"></a>
 // var elements = document.getElementsByTagName('input');
@@ -12,16 +13,24 @@ var elements = document.getElementsByTagName('a');
 
 
 for (var j = 0; j < elements.length; j++) {
-	/*var attr = elements[j].attributes;
-	for (var i = 0; i < attr.length; i++) {
-		var out = attr[i].name + " : " + attr[i].value;
-		console.log(out);
-	}*/
 	
-	var out = "Link " + j + ": " + elements[j].getAttribute('href');
+	var element = elements[j];
+	var out = "Link " + j + ": " + element.getAttribute('href');
 	console.log(out);
 	
+	for (var i = 0; i < element.childNodes.length; i++){
 	
+		var node = element.childNodes[i];
+		if ('IMG' == element.childNodes[i].nodeName){
+			console.log("href img");
+		}
+		if (3 === node.nodeType){
+			var text = node.nodeValue;
+			var newText = text + " " + j;
+            element.replaceChild(document.createTextNode(newText), node);
+		}
+	}
+
 }
 
 // window.location.href = '';
